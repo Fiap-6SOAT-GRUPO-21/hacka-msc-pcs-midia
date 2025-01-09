459d94494ebf2be80a168a7a20a88cff99e6a756
Guilherme Costa Sales <guilhermecosales@gmail.com>
2025-01-09 17:51:55 -0300

    chore: create base project

---
+++ src/main/java/br/com/fiap/pcsmidia/HackaMscPcsMidiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pcsmidia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaMscPcsMidiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackaMscPcsMidiaApplication.class, args);
+    }
+
+}
